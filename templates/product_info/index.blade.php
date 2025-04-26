3878cf71e334787b02f0ca53d0b04bf983a34231
David Blanchard <dpjb@hotmail.fr>
2025-04-26 23:31:10 +0200

    Convert to Twig files.

--- templates/product_info/index.blade.php
+++ templates/product_info/index.blade.php
@@ -1,14 +1,13 @@
-<div class="container marketing" >
-    <div class='row featurette'>
-        <div class='col-md-7 order-md-2'>
-            @include('product_info.top_panel')
-            @include('product_info.tabs_panel')
-            @include('product_info.quantity')
+<div class="container marketing">
+    <div class="row featurette">
+        <div class="col-md-7 order-md-2">
+            {% include 'product_info/top_panel.html.twig' %}
+            {% include 'product_info/tabs_panel.html.twig' %}
+            {% include 'product_info/quantity.html.twig' %}
         </div>
 
-        <div class='col-md-5 order-md-1'>
-            @include('product_info.images_panel')
+        <div class="col-md-5 order-md-1">
+            {% include 'product_info/images_panel.html.twig' %}
         </div>
-
     </div>
 </div>
