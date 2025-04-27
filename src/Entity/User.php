abbbcaac2853ae6d494401beeee4cd81a90d673a
David Blanchard <dpjb@hotmail.fr>
2025-04-28 01:32:49 +0200

    Update User classes.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,37 +1,159 @@
 <?php
-// src/Entity/User.php
+
 namespace App\Entity;
 
+use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-/**
- * @ORM\Entity
- */
-class User
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    // Other fields...
+
+    public const USER_ROLE = 'ROLE_USER';
+    public const ADMIN_ROLE = 'ROLE_ADMIN';
+    public const ROLES = [
+        self::USER_ROLE,
+        self::ADMIN_ROLE,
+    ];
+    public const DEFAULT_ROLE = self::USER_ROLE;
+    public const DEFAULT_ROLES = [self::USER_ROLE];
+    public const DEFAULT_ADMIN_ROLE = self::ADMIN_ROLE;
+    public const DEFAULT_ADMIN_ROLES = [self::ADMIN_ROLE];
+    public const DEFAULT_ROLES_LIST = [
+        self::USER_ROLE => 'User',
+        self::ADMIN_ROLE => 'Admin',
+    ];
+    public const DEFAULT_ROLES_LIST_ADMIN = [
+        self::USER_ROLE => 'User',
+        self::ADMIN_ROLE => 'Admin',
+    ];
+    public const DEFAULT_ROLES_LIST_ALL = [
+        self::USER_ROLE => 'User',
+        self::ADMIN_ROLE => 'Admin',
+    ];
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $email = null;
+
+    
+    #[ORM\Column(length: 180)]
+    private ?string $verificationToken = null;
 
     /**
-     * @ORM\Column(type="boolean")
+     * @var list<string> The user roles
      */
-    private $isVerified = false;
+    #[ORM\Column]
+    private array $roles = [];
 
     /**
-     * @ORM\Column(type="string", nullable=true)
+     * @var string The hashed password
      */
-    private $verificationToken;
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column]
+    private bool $isVerified = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     *
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
 
     public function isVerified(): bool
     {
         return $this->isVerified;
     }
 
-    public function setIsVerified(bool $isVerified): self
+    public function setIsVerified(bool $isVerified): static
     {
         $this->isVerified = $isVerified;
+
         return $this;
     }
 
+    
     public function getVerificationToken(): ?string
     {
         return $this->verificationToken;
