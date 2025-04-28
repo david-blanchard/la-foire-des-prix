7d0a0116dda3730e77013b0fa8c04a264721c0f9
David Blanchard <dpjb@hotmail.fr>
2025-04-28 11:22:45 +0200

    Remove useless Laravel files.

--- resources/views/search/search_nav_bar.blade.php
+++
@@ -1,6 +0,0 @@
-<form id="form0" class="my-2 btn-group" action="javascript:void(0)" method="GET">
-    <input id='search' name='product' class='form-control' type='text' placeholder='Lancez-vous' aria-label='Recherche' />
-    <button type="submit" id='submit-search-cta' class='btn btn-outline-success my-sm-0'>
-        <i class="fa fa-search"></i>
-    </button>
-</form>
