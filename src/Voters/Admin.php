92db714271645e3ea94c78c4d89d86e76e7cba95
David Blanchard <dpjb@hotmail.fr>
2025-04-28 12:12:43 +0200

    Convert some files to Symfony 7.2.

---
+++ src/Voters/Admin.php
@@ -0,0 +1,31 @@
+<?php
+
+     namespace App\Security\Voter;
+
+     use App\Entity\User;
+     use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+     use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+     class AdminVoter extends Voter
+     {
+         private const ADMIN_ROLE = 'ROLE_ADMIN';
+
+         protected function supports(string $attribute, mixed $subject): bool
+         {
+             // Vérifie si l'attribut correspond à ce que le Voter doit gérer
+             return $attribute === self::ADMIN_ROLE;
+         }
+
+         protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+         {
+             $user = $token->getUser();
+
+             if (!$user instanceof User) {
+                 return false; // L'utilisateur n'est pas connecté
+             }
+
+             // Vérifie si l'utilisateur a le rôle ADMIN
+             return $user->getRole() === self::ADMIN_ROLE;
+         }
+     }
+
