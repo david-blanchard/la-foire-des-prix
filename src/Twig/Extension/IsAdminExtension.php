610dfd761b74f915c3387c5b2e4171945621dbfe
David Blanchard <dpjb@hotmail.fr>
2025-05-03 00:15:06 +0200

    Add useless Twig extension for the sake of knowledge.

---
+++ src/Twig/Extension/IsAdminExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Twig\Extension;
+
+use App\Twig\Runtime\IsAdminRuntime;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class IsAdminExtension extends AbstractExtension
+{
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('isAdmin', [IsAdminRuntime::class, 'userIsAdmin']),
+        ];
+    }
+}
---
+++ src/Twig/Runtime/IsAdminRuntime.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Twig\Runtime;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionFactory;
+use Twig\Extension\RuntimeExtensionInterface;
+
+
+class IsAdminRuntime implements RuntimeExtensionInterface
+{
+    public function __construct(
+    ) {
+    }
+
+    public function userIsAdmin(): bool
+    {
+        $session = new Session();
+
+        $roles = $session->get('user')?->getRoles();
+        if (is_array($roles) && isset($roles['ROLE_ADMIN'])) {
+            return true;
+        }
+
+        return false;
+    }
+}
