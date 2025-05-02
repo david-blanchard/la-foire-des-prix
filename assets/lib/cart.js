92849bc68bd104ee4c760f19bffe9acadef17f12
David Blanchard <dpjb@hotmail.fr>
2025-05-02 02:11:21 +0200

    Rearrange assets to prevent accidental loss.

---
+++ assets/lib/cart.js
@@ -0,0 +1,53 @@
+import ServerSession from "./server-session.js"
+
+export default class Cart {
+    constructor() {
+        this.cartSumComponent = document.querySelector(
+            'span[id="cart-cta"] span[id="cart-total"][name="price"]'
+        );
+        this.cartCountComponent = document.querySelector(
+            'span[id="cart-cta"] span[id="cart-count"][name="quantity"]'
+        );
+
+        this.priceComponent = document.querySelector(
+            'h3[id="price-tag"][name="unit-price"]'
+        );
+    }
+
+    update(data) {
+        this.cartCountComponent.innerText = data.quantity
+        this.cartSumComponent.innerText = data.total.toFixed(2).replace('.', ',')
+    }
+
+    retrieve() {
+        const context = this;
+        const session = new ServerSession('/cart');
+        session.retrieve(
+            {
+                type: "Cart",
+            },
+            function (data) {
+                context.update(data)
+            }
+        )
+    }
+
+    store(productId, quantity) {
+        const context = this;
+        const session = new ServerSession('/cart');
+        session.store(
+            {
+                type: "Cart",
+                content: [
+                    {
+                        productId: productId,
+                        quantity: quantity,
+                    },
+                ],
+            },
+            function (data) {
+                context.update(data)
+            }
+        )
+    }
+}
---
+++ assets/lib/server-session.js
@@ -0,0 +1,43 @@
+export default class ServerSession {
+
+    #baseApiUrl = '/session';
+
+    constructor(baseApiUrl) {
+        this.#baseApiUrl = baseApiUrl;
+        $.ajaxSetup({
+            headers: {
+              'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    }
+
+    retrieve(data, callback) {
+        const context = this
+        $.ajax({
+            type: "POST",
+            url: this.#baseApiUrl + "/retrieve",
+            data: data,
+            success: (data) => {
+                if ("function" == typeof callback) {
+                    callback.call(context, data)
+                }
+            },
+            dataType: "json",
+        })
+    }
+
+    store(data, callback) {
+        const context = this
+        $.ajax({
+            type: "POST",
+            url: this.#baseApiUrl + "/store",
+            data: data,
+            success: (data) => {
+                if ("function" == typeof callback) {
+                    callback.call(context, data)
+                }
+            },
+            dataType: "json",
+        })
+    }
+}
