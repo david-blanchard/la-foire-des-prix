57eaebc7786d29e748d41d99365de94e6bcb3c8c
David Blanchard <dpjb@hotmail.fr>
2025-05-10 11:30:14 +0200

    Refactor controllers and manage 404.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,75 +2,18 @@
 
 namespace App\Controller;
 
-use App\Repository\ProductRepository;
-use App\Service\CartService;
-use App\Service\ProductService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/')]
 class HomeController extends AbstractController
 {
-    public function __construct(
-        private readonly ProductRepository $productRepository,
-        private readonly CartService       $cartService,
-        private readonly ProductService    $productService,
-    ) {
-    }
-
     #[Route('/', name: 'home', methods: ['GET'])]
     public function index(): Response
     {
-        // Attempt to fetch properties from a cache by ID
-        $props = $this->productRepository->getPropertiesFromCacheById(-1);
-        if ($props !== null) {
-            return $this->render('home/index.html.twig', $props);
-        }
-
-        // Fetch attributes and convert them to properties
-        $product = $this->productRepository->findById();
-        $props = $this->productService->prepareViewFields($product);
-
-        $cartFields = $this->cartService->prepareViewFields();
-
-        // Store properties in a cache
-        $this->productRepository->putPropertiesInCacheById(-1, $props);
-
-        return $this->render('home/index.html.twig', [
-            ...$props,
-            ...$cartFields,
-        ]);
+        return new RedirectResponse('/mode-femme/' );
     }
 
-    #[Route('/mode-femme/{slug}', name: 'product_info', methods: ['GET'])]
-    #[Route('/recherche/{slug}', name: 'search', methods: ['GET'])]
-    public function show(string $slug): Response
-    {
-        // Attempt to fetch properties from a cache by slug
-        $props = $this->productRepository->getPropertiesFromCacheBySlug($slug);
-        if ($props !== null) {
-            return $this->render('home/index.html.twig', $props);
-        }
-
-        // Fetch the product by slug
-        $product = $this->productRepository->findOneBySlug($slug);
-
-        if ($product === null) {
-            throw $this->createNotFoundException('Product not found.');
-        }
-
-        // Fetch attributes and convert them to properties
-        $product = $this->productRepository->findById($product->getId());
-        $props = $this->productService->prepareViewFields($product);
-
-        // Store properties in a cache
-        $this->productRepository->putPropertiesInCacheBySlug($slug, $props);
-
-        $cartFields = $this->cartService->prepareViewFields();
-
-        return $this->render('home/index.html.twig', [
-            ...$props,
-            ...$cartFields,
-        ]);
-    }
 }
--- tests/Feature/RouteTest.php
+++ tests/Feature/RouteTest.php
@@ -7,89 +7,89 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RouteTest extends WebTestCase
 {
-//    private KernelBrowser $client;
-//
-//    private const ADMIN_EMAIL = 'admin@example.com';
-//    private const ADMIN_PASSWORD = 'password';
-//
-//    protected function setUp(): void
-//    {
-//        parent::setUp();
-//        $this->client = self::createClient();
-//    }
-//
-//    public function test_modeFemmePageIsFound(): void
-//    {
-//        $this->client->request('GET', '/mode-femme/');
-//
-//        $this->assertResponseIsSuccessful();
-//    }
-//
-//    public function test_modeFemmePageWithValidSlugVesteIsFound(): void
-//    {
-//        $this->client->request('GET', '/mode-femme/veste');
-//
-//        $this->assertResponseIsSuccessful();
-//    }
-//
-//    public function test_modeFemmePageWithValidSlugRobeIsFound(): void
-//    {
-//        $this->client->request('GET', '/mode-femme/robe');
-//
-//        $this->assertResponseIsSuccessful();
-//    }
-//
-//    public function test_modeFemmePageWithValidSlugMailleIsFound(): void
-//    {
-//        $this->client->request('GET', '/mode-femme/maille');
-//
-//        $this->assertResponseIsSuccessful();
-//    }
-//
-//    public function test_modeFemmePageWithInvalidSlugPantalonIs404(): void
-//    {
-//        $this->client->request('GET', '/mode-femme/pantalon');
-//
-//        $this->assertResponseStatusCodeSame(404);
-//    }
-//
-//    public function test_adminUiRedirectToLoginAsGuest(): void
-//    {
-//        $this->client->request('GET', '/admin/');
-//
-//        $this->assertResponseRedirects('/login');
-//    }
-//
-//    public function test_adminUiRequestAsAdminIsValid(): void
-//    {
-//        $this->client = self::createClient([], [
-//            'PHP_AUTH_USER' => self::ADMIN_EMAIL,
-//            'PHP_AUTH_PW'   => self::ADMIN_PASSWORD,
-//        ]);
-//        $this->client->request('GET', '/admin/');
-//
-//        $this->assertResponseIsSuccessful();
-//    }
-//
-//    public function test_adminUiEditProductOneIsValid(): void
-//    {
-//        $this->client = self::createClient([], [
-//            'PHP_AUTH_USER' => self::ADMIN_EMAIL,
-//            'PHP_AUTH_PW'   => self::ADMIN_PASSWORD,
-//        ]);
-//        $this->client->request('GET', '/admin/products/1/edit');
-//
-//        $this->assertResponseIsSuccessful();
-//    }
-//
-//    public function test_adminUiInvalidCredentials(): void
-//    {
-//        $this->client = self::createClient([], [
-//            'PHP_AUTH_USER' => 'invalid@example.com',
-//            'PHP_AUTH_PW'   => 'wrongpassword',
-//        ]);
-//        $this->client->request('GET', '/admin/');
-//
-//        $this->assertResponseStatusCodeSame(401);
-//    }
+    private KernelBrowser $client;
+
+    private const ADMIN_EMAIL = 'admin@lfdp.fr';
+    private const ADMIN_PASSWORD = 'demo';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->client = self::createClient();
+    }
+
+    public function test_modeFemmePageIsFound(): void
+    {
+        $this->client->request('GET', '/mode-femme/');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function test_modeFemmePageWithValidSlugVesteIsFound(): void
+    {
+        $this->client->request('GET', '/mode-femme/veste');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function test_modeFemmePageWithValidSlugRobeIsFound(): void
+    {
+        $this->client->request('GET', '/mode-femme/robe');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function test_modeFemmePageWithValidSlugMailleIsFound(): void
+    {
+        $this->client->request('GET', '/mode-femme/maille');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function test_modeFemmePageWithInvalidSlugPantalonIs404(): void
+    {
+        $this->client->request('GET', '/mode-femme/pantalon');
+
+        $this->assertResponseStatusCodeSame(404);
+    }
+
+    public function test_adminUiRedirectToLoginAsGuest(): void
+    {
+        $this->client->request('GET', '/admin/');
+
+        $this->assertResponseRedirects('/login');
+    }
+
+    public function test_adminUiRequestAsAdminIsValid(): void
+    {
+        $this->client = self::createClient([], [
+            'email' => self::ADMIN_EMAIL,
+            'password'   => self::ADMIN_PASSWORD,
+        ]);
+        $this->client->request('POST', '/admin/');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function test_adminUiEditProductOneIsValid(): void
+    {
+        $this->client = self::createClient([], [
+            'email' => self::ADMIN_EMAIL,
+            'password'   => self::ADMIN_PASSWORD,
+        ]);
+        $this->client->request('POST', '/admin/products/1/edit');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function test_adminUiInvalidCredentials(): void
+    {
+        $this->client = self::createClient([], [
+            'email' => 'invalid@example.com',
+            'password'   => 'wrongpassword',
+        ]);
+        $this->client->request('POST', '/admin/');
+
+        $this->assertResponseStatusCodeSame(401);
+    }
 }
