c1b5076bc8631adf18f1e91e74cb18302ed7e504
David Blanchard <dpjb@hotmail.fr>
2025-08-17 14:19:13 +0200

    Clean code

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,6 @@
-import { useState } from 'react';
 import './Style/App.css';
 import './Style/Popin.css';
-import WomanPage from './Components/Woman/WomanPage.tsx';
+import WomanPage from './Components/Woman/WomanPage';
 
 function App() {
   return <WomanPage />;
