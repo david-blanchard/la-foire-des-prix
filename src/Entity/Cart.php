acf7b69aa1f923affc3494aaffcc919608efff1a
David Blanchard <dpjb@hotmail.fr>
2025-08-24 00:52:08 +0200

    Add Cart entity, to be worked on.

---
+++ src/Entity/Cart.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\CartRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+#[ORM\Entity(repositoryClass: CartRepository::class)]
+#[ApiResource]
+class Cart
+{
+    use Traits\Identifier;
+    use TimestampableEntity;
+
+    #[ORM\Column]
+    private ?float $total = null;
+
+    #[ORM\Column]
+    private ?int $count = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    private ?User $client = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTotal(): ?float
+    {
+        return $this->total;
+    }
+
+    public function setTotal(float $total): static
+    {
+        $this->total = $total;
+
+        return $this;
+    }
+
+    public function getCount(): ?int
+    {
+        return $this->count;
+    }
+
+    public function setCount(int $count): static
+    {
+        $this->count = $count;
+
+        return $this;
+    }
+
+    public function getClient(): ?User
+    {
+        return $this->client;
+    }
+
+    public function setClient(?User $client): static
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+}
