ba62ed5d0ffd61a8121350449f50d5c74512dcd1
David Blanchard <dpjb@hotmail.fr>
2025-11-27 14:31:12 +0100

    fix: limit product relationship depth with productimage

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -10,6 +10,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
 use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ApiResource(
